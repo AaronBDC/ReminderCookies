edef5e8743a0d56af506b3d74d9e7ead4841e8b8
Aaron DC <aaronbdc@mail.com>
2017-06-01 09:14:12 -0400

    testing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
-import '../App.css';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addReminder} from '../actions';
+
+
+import '../App.css';  //not sure if this line is needed
 
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  addReminder(){
+//    console.log('this.state', this.state);
+    console.log('this', this);
+  }
   render(){
     return (
       <div className="App">
@@ -15,11 +31,13 @@ class App extends Component {
             <input
               className="form-control"
               placeholder="I have to..."
+              onChange={event => this.setState({text: event.target.value})}
               />
           </div>
           <button
             type="button"
             className="btn btn-success"
+            onClick={event => this.setSTate({text: event.target.value})}
             >
             Add Reminder
           </button>
@@ -32,4 +50,8 @@ class App extends Component {
   }
 
 }
-export default App;
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({addReminder}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(App);
